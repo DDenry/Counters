7d876e9df4e8e14b3972cf4e74da7d0c62aad9f0
DESKTOP-MEJ05BQ\DDenry <dengrui.520@163.com>
2017-12-18 11:12:15 +0800

    搭建UI中，实现右下角拖拉窗口大小。

--- Counters/Form1.Designer.cs
+++ Counters/Form1.Designer.cs
@@ -32,19 +32,27 @@
             this.panel_bottom = new System.Windows.Forms.Panel();
             this.pictureBox_sizeEditable = new System.Windows.Forms.PictureBox();
             this.splitContainer_content = new System.Windows.Forms.SplitContainer();
+            this.treeView_menu = new System.Windows.Forms.TreeView();
+            this.panel_controllButton = new System.Windows.Forms.Panel();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_sizable = new System.Windows.Forms.Button();
+            this.panel_title.SuspendLayout();
             this.panel_bottom.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sizeEditable)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer_content)).BeginInit();
+            this.splitContainer_content.Panel1.SuspendLayout();
             this.splitContainer_content.SuspendLayout();
+            this.panel_controllButton.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_title
             // 
             this.panel_title.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel_title.Controls.Add(this.panel_controllButton);
             this.panel_title.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel_title.Location = new System.Drawing.Point(0, 0);
             this.panel_title.Name = "panel_title";
-            this.panel_title.Size = new System.Drawing.Size(1022, 80);
+            this.panel_title.Size = new System.Drawing.Size(1095, 80);
             this.panel_title.TabIndex = 0;
             this.panel_title.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownEvent);
             this.panel_title.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMoveEvent);
@@ -57,19 +65,22 @@
             this.panel_bottom.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel_bottom.Location = new System.Drawing.Point(0, 610);
             this.panel_bottom.Name = "panel_bottom";
-            this.panel_bottom.Size = new System.Drawing.Size(1022, 60);
+            this.panel_bottom.Size = new System.Drawing.Size(1095, 60);
             this.panel_bottom.TabIndex = 0;
             // 
             // pictureBox_sizeEditable
             // 
+            this.pictureBox_sizeEditable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox_sizeEditable.Image = global::Counters.Properties.Resources.size1;
-            this.pictureBox_sizeEditable.Location = new System.Drawing.Point(1002, 40);
+            this.pictureBox_sizeEditable.Location = new System.Drawing.Point(1075, 40);
             this.pictureBox_sizeEditable.Name = "pictureBox_sizeEditable";
             this.pictureBox_sizeEditable.Size = new System.Drawing.Size(20, 20);
             this.pictureBox_sizeEditable.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox_sizeEditable.TabIndex = 0;
             this.pictureBox_sizeEditable.TabStop = false;
+            this.pictureBox_sizeEditable.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownEvent);
             this.pictureBox_sizeEditable.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMoveEvent);
+            this.pictureBox_sizeEditable.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUpEvent);
             // 
             // splitContainer_content
             // 
@@ -81,24 +92,76 @@
             // 
             // splitContainer_content.Panel1
             // 
+            this.splitContainer_content.Panel1.Controls.Add(this.treeView_menu);
             this.splitContainer_content.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.splitContainer_content.Panel1MinSize = 120;
+            this.splitContainer_content.Panel1MinSize = 150;
             // 
             // splitContainer_content.Panel2
             // 
+            this.splitContainer_content.Panel2.BackColor = System.Drawing.Color.Transparent;
             this.splitContainer_content.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.splitContainer_content.Panel2MinSize = 898;
+            this.splitContainer_content.Panel2MinSize = 868;
             this.splitContainer_content.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.splitContainer_content.Size = new System.Drawing.Size(1022, 530);
-            this.splitContainer_content.SplitterDistance = 120;
+            this.splitContainer_content.Size = new System.Drawing.Size(1095, 530);
+            this.splitContainer_content.SplitterDistance = 223;
             this.splitContainer_content.TabIndex = 2;
+            // 
+            // treeView_menu
+            // 
+            this.treeView_menu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView_menu.Location = new System.Drawing.Point(0, 0);
+            this.treeView_menu.Name = "treeView_menu";
+            this.treeView_menu.Size = new System.Drawing.Size(223, 530);
+            this.treeView_menu.TabIndex = 0;
+            // 
+            // panel_controllButton
+            // 
+            this.panel_controllButton.Controls.Add(this.button_sizable);
+            this.panel_controllButton.Controls.Add(this.button_exit);
+            this.panel_controllButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel_controllButton.Location = new System.Drawing.Point(935, 0);
+            this.panel_controllButton.Name = "panel_controllButton";
+            this.panel_controllButton.Size = new System.Drawing.Size(160, 80);
+            this.panel_controllButton.TabIndex = 0;
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button_exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_exit.FlatAppearance.BorderSize = 0;
+            this.button_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_exit.Font = new System.Drawing.Font("宋体", 20F);
+            this.button_exit.ForeColor = System.Drawing.Color.DimGray;
+            this.button_exit.Location = new System.Drawing.Point(80, 0);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(80, 80);
+            this.button_exit.TabIndex = 0;
+            this.button_exit.Text = "×";
+            this.button_exit.UseVisualStyleBackColor = false;
+            this.button_exit.Click += new System.EventHandler(this.ExitForm);
+            // 
+            // button_sizable
+            // 
+            this.button_sizable.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button_sizable.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_sizable.FlatAppearance.BorderSize = 0;
+            this.button_sizable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_sizable.Font = new System.Drawing.Font("宋体", 20F);
+            this.button_sizable.ForeColor = System.Drawing.Color.DimGray;
+            this.button_sizable.Location = new System.Drawing.Point(0, 0);
+            this.button_sizable.Name = "button_sizable";
+            this.button_sizable.Size = new System.Drawing.Size(80, 80);
+            this.button_sizable.TabIndex = 1;
+            this.button_sizable.Text = "口";
+            this.button_sizable.UseVisualStyleBackColor = false;
+            this.button_sizable.Click += new System.EventHandler(this.MinOrMaxForm);
             // 
             // MainActivity
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1022, 670);
+            this.ClientSize = new System.Drawing.Size(1095, 670);
             this.ControlBox = false;
             this.Controls.Add(this.splitContainer_content);
             this.Controls.Add(this.panel_bottom);
@@ -107,10 +170,14 @@
             this.Name = "MainActivity";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Resize += new System.EventHandler(this.FormResize);
+            this.panel_title.ResumeLayout(false);
             this.panel_bottom.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sizeEditable)).EndInit();
+            this.splitContainer_content.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer_content)).EndInit();
             this.splitContainer_content.ResumeLayout(false);
+            this.panel_controllButton.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -121,6 +188,10 @@
         private System.Windows.Forms.Panel panel_bottom;
         private System.Windows.Forms.SplitContainer splitContainer_content;
         private System.Windows.Forms.PictureBox pictureBox_sizeEditable;
+        private System.Windows.Forms.TreeView treeView_menu;
+        private System.Windows.Forms.Panel panel_controllButton;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_sizable;
     }
 }
 
--- Counters/Form1.cs
+++ Counters/Form1.cs
@@ -15,6 +15,10 @@ namespace Counters
         private Boolean canMoveForm = false;
         private Point preLocation;
 
+        private Boolean canSetSize = false;
+
+        protected const int minWidth = 1022;
+        protected const int minHeight = 670;
 
         public MainActivity()
         {
@@ -24,43 +28,110 @@ namespace Counters
         //
         private void MouseDownEvent(object sender, MouseEventArgs e)
         {
-            //按下鼠标左键
-            if (e.Button.Equals(MouseButtons.Left))
+            switch (sender.GetType().Name)
             {
-                canMoveForm = true;
-                preLocation = e.Location;
-            }
-            //按下鼠标右键
-            else if (e.Button.Equals(MouseButtons.Right)) {
+                case "Panel":
+                    //按下鼠标左键
+                    if (e.Button.Equals(MouseButtons.Left))
+                    {
+                        canMoveForm = true;
+                        preLocation = e.Location;
+                    }
+                    //按下鼠标右键
+                    else if (e.Button.Equals(MouseButtons.Right))
+                    {
 
+                    }
+                    break;
+                case "PictureBox":
+                    if (e.Button.Equals(MouseButtons.Left))
+                    {
+                        canSetSize = true;
+                    }
+                    break;
             }
+
         }
 
         private void MouseMoveEvent(object sender, MouseEventArgs e)
         {
-            //移动窗体
-            if (e.Button.Equals(MouseButtons.Left) && canMoveForm)
+            switch (sender.GetType().Name)
             {
-                Point newPosition = new Point(e.Location.X - preLocation.X, e.Location.Y - preLocation.Y);
-                this.Location += new Size(newPosition);
+                case "Panel":
+                    //移动窗体
+                    if (e.Button.Equals(MouseButtons.Left) && canMoveForm)
+                    {
+                        if (this.WindowState != FormWindowState.Normal)
+                        {
+                            float X = preLocation.X * 1.0f / this.Width;
+                            float Y = preLocation.Y * 1.0f / this.Height;
+                            this.WindowState = FormWindowState.Normal;
+                            this.Location = (Point)new Size((int)(e.Location.X - this.Width * X), (int)(e.Location.Y - this.Height * Y));
+                            preLocation = e.Location;
+                        }
+                        else
+                        {
+                            Point newPosition = new Point(e.Location.X - preLocation.X, e.Location.Y - preLocation.Y);
+                            this.Location += new Size(newPosition);
+                        }
+                    }
+                    break;
+                case "PictureBox":
+                    //
+                    if (pictureBox_sizeEditable.Width - e.Location.X <= 10 && pictureBox_sizeEditable.Height - e.Location.Y <= 10)
+                    {
+                        this.Cursor = Cursors.SizeNWSE;
+                        if (canSetSize)
+                            //调整窗体大小
+                            ResizeForm();
+                    }
+                    else if (canSetSize)
+                    {
+                        this.Cursor = Cursors.SizeNWSE;
+                        //
+                        ResizeForm();
+                    }
+                    else this.Cursor = Cursors.Arrow;
+                    break;
             }
+        }
+        //
+        private void ResizeForm()
+        {
+            this.Cursor = Cursors.SizeNWSE;
             //
-            if (pictureBox_sizeEditable.Width - e.Location.X <= 5 && pictureBox_sizeEditable.Height - e.Location.Y <= 5)
-            {
-                this.Cursor = Cursors.SizeNWSE;
-            }
-            else this.Cursor = Cursors.Arrow;
+            if (MousePosition.X - this.Left < minWidth) this.Width = minWidth;
+            else this.Width = MousePosition.X - this.Left;
+            if (MousePosition.Y - this.Top < minHeight) this.Height = minHeight;
+            else this.Height = MousePosition.Y - this.Top;
         }
 
         private void MouseUpEvent(object sender, MouseEventArgs e)
         {
-            //抬起鼠标左键
-            if (e.Button.Equals(MouseButtons.Left))
+            canMoveForm = false;
+            canSetSize = false;
+
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void FormResize(object sender, EventArgs e)
+        {
+            //
+            if (splitContainer_content.Panel1.Width > this.Width - splitContainer_content.Panel2.Width - splitContainer_content.SplitterWidth)
             {
-                canMoveForm = false;
+                splitContainer_content.SplitterDistance = this.Width - splitContainer_content.Panel2.Width - splitContainer_content.SplitterWidth;
             }
         }
-        //
 
+        private void ExitForm(object sender, EventArgs e)
+        {
+            //
+            this.Close();
+        }
+
+        private void MinOrMaxForm(object sender, EventArgs e)
+        {
+            this.WindowState = (this.WindowState == FormWindowState.Maximized ? this.WindowState = FormWindowState.Normal : this.WindowState = FormWindowState.Maximized);
+        }
     }
 }
